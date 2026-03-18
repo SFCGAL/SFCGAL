9800c2225cc5305eeaaf2d478a48c18fdfbe9f1c
Jean Felder <jean.felder@oslandia.com>
2026-03-18 15:38:16 +0100

    feat(algorithm): Add split3D algorithm

---
+++ src/detail/algorithm/split3D.h
@@ -0,0 +1,34 @@
+// Copyright (c) 2026-2026, SFCGAL team.
+// SPDX-License-Identifier: LGPL-2.0-or-later
+
+#ifndef SFCGAL_ALGORITHM_DETAIL_SPLIT_3D_H_
+#define SFCGAL_ALGORITHM_DETAIL_SPLIT_3D_H_
+
+#include <memory>
+
+#include "SFCGAL/GeometryCollection.h"
+#include "SFCGAL/Kernel.h"
+
+namespace SFCGAL::algorithm::detail {
+
+/**
+ * Split the given geometry with a plane.
+ * Based on CGAL::PMP::split operator
+ *
+ * @param geometry The input geometry to split.
+ * @param plane The splitting plane
+ * @param closeGeometries If true, ensures resulting geometries are closed.
+ *   In the case of a SOLID, this parameter is always forced to true,
+ *   since a solid is a closed geometry.
+ *
+ * @return A unique pointer to a GeometryCollection of the split parts
+ * or the original geometry if the plane does not intersect it.
+ */
+template <typename geomType>
+auto
+split3D(const geomType &geometry, const CGAL::Plane_3<Kernel> &plane,
+        bool closeGeometries) -> std::unique_ptr<GeometryCollection>;
+
+} // namespace SFCGAL::algorithm::detail
+
+#endif // SFCGAL_ALGORITHM_DETAIL_SPLIT_3D_H_
