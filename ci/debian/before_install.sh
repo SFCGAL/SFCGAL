94657ec16cbae3b96448dde0b963998e1d83c8a5
bdm-oslandia <benoit.de.mezzo@oslandia.com>
2026-04-01 12:36:14 +0200

    chore: improve before_install script for failure

--- ci/debian/before_install.sh
+++ ci/debian/before_install.sh
@@ -1,15 +1,31 @@
-export DEBIAN_FRONTEND=noninteractive
-apt-get update -qq
-apt-get install --yes \
-    ca-certificates cmake libboost-program-options-dev \
-    libboost-test-dev libboost-thread-dev nlohmann-json3-dev \
-    libboost-system-dev libboost-serialization-dev \
-    libmpfr-dev libgmp-dev libeigen3-dev \
-    xz-utils
-#CGAL
+#!/bin/env bash
 
-wget https://github.com/CGAL/cgal/releases/download/v"$1"/CGAL-"$1".tar.xz
-tar xJf CGAL-"$1".tar.xz
-cd CGAL-"$1" && mkdir build && cd build && cmake -DCMAKE_INSTALL_PREFIX=$CI_PROJECT_DIR/CGAL .. && make && make install && cd ../..
+if [ -z "$1" ]; then
+	echo "Missing CGAL version parameter. Exit!"
+	exit 1
+fi
+
+if [ -z "$CI_PROJECT_DIR" ]; then
+	echo "Missing env var CI_PROJECT_DIR. Exit!"
+	exit 1
+fi
+
+export DEBIAN_FRONTEND=noninteractive
+apt-get update -qq || exit 1
+apt-get install --yes \
+	ca-certificates cmake libboost-program-options-dev \
+	libboost-test-dev libboost-thread-dev nlohmann-json3-dev \
+	libboost-system-dev libboost-serialization-dev \
+	libmpfr-dev libgmp-dev libeigen3-dev \
+	xz-utils ||
+	exit 1
 
 cmake --version
+
+#CGAL
+wget https://github.com/CGAL/cgal/releases/download/v"$1"/CGAL-"$1".tar.xz || exit 1
+tar xJf CGAL-"$1".tar.xz || exit 1
+cd CGAL-"$1" && mkdir build && cd build || exit 1
+cmake -DCMAKE_INSTALL_PREFIX="$CI_PROJECT_DIR/CGAL" .. || exit 1
+make || exit 1
+make install || exit 1
--- ci/fedora/before_install.sh
+++ ci/fedora/before_install.sh
@@ -1,11 +1,24 @@
-sudo yum update -qy
+#!/bin/env bash
+
+if [ -z "$1" ]; then
+	echo "Missing CGAL version parameter. Exit!"
+	exit 1
+fi
+
+if [ -z "$CI_PROJECT_DIR" ]; then
+	echo "Missing env var CI_PROJECT_DIR. Exit!"
+	exit 1
+fi
+
+sudo yum update -qy || exit 1
 sudo yum install -y \
-    cmake boost boost-devel gmp gmp-c++ gmp-devel mpfr mpfr-devel make json-devel eigen3-devel \
-    xz
+	cmake boost boost-devel gmp gmp-c++ gmp-devel mpfr mpfr-devel make json-devel eigen3-devel \
+	xz || exit 1
 
 #CGAL
-wget https://github.com/CGAL/cgal/releases/download/v"$1"/CGAL-"$1".tar.xz
-tar xJf CGAL-"$1".tar.xz
-cd CGAL-"$1" && mkdir build && cd build && cmake -DCMAKE_INSTALL_PREFIX=$CI_PROJECT_DIR/CGAL .. && make && make install && cd ../..
-
-cmake --version
+wget https://github.com/CGAL/cgal/releases/download/v"$1"/CGAL-"$1".tar.xz || exit 1
+tar xJf CGAL-"$1".tar.xz || exit 1
+cd CGAL-"$1" && mkdir build && cd build || exit 1
+cmake -DCMAKE_INSTALL_PREFIX="$CI_PROJECT_DIR/CGAL" .. || exit 1
+make || exit 1
+make install || exit 1
