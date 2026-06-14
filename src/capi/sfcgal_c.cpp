c6892f8bc39249836a97f27e9abdc2c784fc9314
Jean Felder <jean.felder@gmail.com>
2026-06-14 11:46:36 +0200

    refactor(capi): Replace deprecated filesystem::u8path by filesystem::path
    It has been deprecated since c++20.

--- src/capi/sfcgal_c.cpp
+++ src/capi/sfcgal_c.cpp
@@ -670,7 +670,9 @@ extern "C" auto
 sfcgal_io_read_obj_file(const char *filename) -> sfcgal_geometry_t *
 {
   SFCGAL_GEOMETRY_CONVERT_CATCH_TO_ERROR(
-      return SFCGAL::io::OBJ::loadFromFile(std::filesystem::u8path(filename))
+      return SFCGAL::io::OBJ::loadFromFile(
+                 std::filesystem::path(std::u8string(
+                     reinterpret_cast<const char8_t *>(filename))))
           .release();)
 }
 
