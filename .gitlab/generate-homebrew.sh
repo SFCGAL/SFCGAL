3e36e518a7bebc4bb917554189efa8d6594a3e82
Jean Felder <jean.felder@gmail.com>
2026-06-14 17:13:45 +0200

    ci: Publish homebrew packages to the registry instead of the website

--- .gitlab/generate-homebrew.sh
+++ .gitlab/generate-homebrew.sh
@@ -4,16 +4,17 @@ set -e
 
 INSTALL_DIR="$1"
 CURRENT_VERSION="$2"
+PACKAGE_VERSION="$3"
 
-ARCHIVE="sfcgal--latest.arm64_sequoia.bottle.tar.gz"
+ARCHIVE="sfcgal--${PACKAGE_VERSION}.arm64_sequoia.bottle.tar.gz"
 tar -czvf ${ARCHIVE} -C ${INSTALL_DIR} .
 SHA256=$(sha256sum "${ARCHIVE}" | awk '{print $1}')
 
-cat <<EOF > sfcgal-latest.rb
+cat <<EOF > sfcgal-${PACKAGE_VERSION}.rb
 class Sfcgal < Formula
   desc "SFCGAL is a C++ wrapper library around CGAL with the aim of supporting ISO 19107:2019 and OGC Simple Features for 3D operations."
   homepage "https://sfcgal.org"
-  url "https://sfcgal.gitlab.io/SFCGAL/homebrew/${ARCHIVE}"
+  url "https://gitlab.com/api/v4/projects/19674165/packages/generic/homebrew/${PACKAGE_VERSION}/${ARCHIVE}"
   sha256 "${SHA256}"
   license "LGPL-2.0-or-later"
   version "${CURRENT_VERSION}"
